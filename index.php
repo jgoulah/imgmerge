c29f5907ac7785087e9c9b5c93f328b593c5d0aa
John Goulah <jgoulah@etsy.com>
2012-03-29 20:45:22 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?
+
+if (!isset($_REQUEST['img1']) && !isset($_REQUEST['img2'])) {
+
+?>
+<h1>Merge Graphs!</h1>
+<form>
+    <label for="img1">First Graph URL:</label>
+    <input type="text" name="img1" id="img1" size="100">
+    <br/>
+    <label for="img2">Second Graph URL:</label>
+    <input type="text" name="img2" id="img2" size="100">
+    <br/>
+    <input type="submit" name="submit" id="submit_btn" value="Merge!" />
+</form>
+<?
+
+
+} else {
+
+    ini_set('allow_url_fopen', true);
+
+    $img1 = 'http://ganglia.etsycorp.com/stacked.php?m=mysql_conns_active&c=DBShard&r=hour&st=1333050487&min=0';
+    //$img1 = $_REQUEST['img1']; 
+    $img2 = 'http://ganglia.etsycorp.com/stacked.php?m=mysql_conns&c=DBShard&r=1hours&st=1333050038&min=0';
+    //$img2 = $_REQUEST['img2']; 
+
+    // Create image instances
+    $dest = imagecreatefrompng($img1);
+    $src  = imagecreatefrompng($img2);
+
+    $src_size   = getimagesize($img2);
+    $src_width  = $src_size[0];
+    $src_height = $src_size[1];
+
+
+    imagealphablending($dest, true);
+    imagesavealpha($dest, true);
+
+    // Copy and merge
+    imagecopymerge($dest, $src, 0, 0, 0, 0, $src_width, $src_height, 50);
+
+    // Output and free from memory
+    header('Content-Type: image/gif');
+    imagegif($dest);
+
+    imagedestroy($dest);
+    imagedestroy($src);
+
+}
